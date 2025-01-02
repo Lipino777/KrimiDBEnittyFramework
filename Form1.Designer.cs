63a5837ba12ba96d91ceb6f1074170746017e3c4
Lipino777 <tomaslipa116@gmail.com>
2025-01-02 20:29:41 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace KrimiDBEnittyFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 72;
+            dataGridView1.Size = new Size(1965, 1104);
+            dataGridView1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1979, 1153);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(dataGridView1);
+            tabPage1.Location = new Point(4, 39);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1971, 1110);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 39);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(342, 132);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1979, 1153);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq.Expressions;
+
+
+namespace KrimiDBEnittyFramework
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+            
+            CrDbContext db = new CrDbContext();
+            db.People.Add(new krimiPerson { personFirstName = "Petr", personLastName = "Spacek", personCrimeCount = 1000 });
+            db.SaveChanges();
+            //var temPersonC = db.People.Where(t => t.personFirstName == "petr" && t.personLastName == "Spacek").Select(t => t.personID).FirstOrDefault();
+            var temPersonC = db.People.OrderByDescending(t => t.personID).Select(t => t.personID).FirstOrDefault();
+
+            db.People.Add(new krimiPerson { personFirstName = "Tomas", personLastName = "lipavsky", personCrimeCount = 23 });
+            db.SaveChanges();
+            //var temPersonV = db.People.Where(t => t.personFirstName == "Tomas" && t.personLastName == "lipavsky").Select(t=> t.personID).FirstOrDefault();
+            var temPersonV = db.People.OrderByDescending(t => t.personID).Select(t => t.personID).FirstOrDefault();
+
+            db.Leads.Add(new krimiLead { leadAddress = "Hradecka 119", leadDescription = "Mr Mach knows but doesn't do crap about it" });
+            db.SaveChanges();
+            var temLead = db.Leads.OrderByDescending(t => t.leadID).Select(t => t.leadID).FirstOrDefault();
+
+            db.Investigations.Add(new krimiInvestigation { investigationSuspectID = temPersonC, investigationLeadID = temLead });
+            db.SaveChanges();
+            var temInvestigation = db.Leads.OrderByDescending(t => t.leadID).Select(t => t.leadID).FirstOrDefault();
+
+            db.Crimes.Add(new krimiCrime { crimeAlias = "case of the bully", crimeDescript = "hidden intentional attacks during PE", crimeInvestigID = temInvestigation, crimeVictimID = temPersonV });
+            db.SaveChanges();
+
+
+
+
+
+
+        }
+    }
+    class CrDbContext : DbContext
+    {
+        public CrDbContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=krimiDatabase;Authentication=Active Directory Integrated;");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<krimiCrime> Crimes { get; set; }
+        public virtual DbSet<krimiInvestigation> Investigations { get; set;}
+        public virtual DbSet<krimiLead> Leads { get; set; }
+        public virtual DbSet<krimiPerson> People { get; set; }
+
+    }
+    class krimiCrime()
+    {
+        [Key]
+        public int crimeID { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string crimeAlias { get; set; }
+
+        public int crimeVictimID { get; set; }
+
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string crimeDescript { get; set; }
+
+        public int crimeInvestigID { get; set; }
+    }
+    class krimiInvestigation()
+    {
+        [Key]
+        public int investigationID { get; set; }
+        public int investigationSuspectID { get; set; }
+        public int investigationLeadID { get; set; }
+    }
+    class krimiLead()
+    {
+        [Key]
+        public int leadID { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string leadAddress { get; set; }
+
+        public string leadDescription { get; set; }
+    }
+    class krimiPerson()
+    {
+        [Key]
+        public int personID { get; set;}
+
+        [Column(TypeName ="nvarchar(30)")]
+        public string personFirstName { get; set; }
+        [Column(TypeName = "nvarchar(30)")]
+        public string personLastName { get; set; }
+
+        [Column(TypeName ="image")]
+        public byte[] personFingerprint { get; set; }
+
+        public int personCrimeCount { get; set; }
+    }
+}
